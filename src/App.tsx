2855dec65e0cd2a617ff0a506a4bbd6b4a31029d
Danny Gelman <dannygelman1@gmail.com>
2024-05-08 12:55:20 -0400

    description

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import MirrorDemo from "./MirrorDemo";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <span style={{ maxWidth: "800px", paddingBottom: "10px" }}>
+        Move the person up/down and see how their view in the mirror changes.
+        Press and on the points on the parallel mirrors and try to get the light
+        ray to hit their field of view. Can you see the relationship between the
+        light ray and the position of the triangle in the reflection or between
+        your position and your field of view?
+      </span>
       <MirrorDemo />
     </div>
   );
